b3d07cbb43397f0b25ec37ab431c83ec3cde2c5b
statelessthefilm <79004304+statelessthefilm@users.noreply.github.com>
2021-03-03 15:46:15 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -231,7 +231,7 @@ function centerContent()
 -----------------------------------------------------------*/
 
       var tag = document.createElement('script');
-      tag.src = "http://www.youtube.com/player_api";
+      tag.src = "https://www.youtube.com/player_api";
       var firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
       var player;
